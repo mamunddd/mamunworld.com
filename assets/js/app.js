a00f41f520efd1a9cbb3cad2678fe3416d2de8d6
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2022-12-23 22:46:00 +0600

    add my resume section and some js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,11 @@ window.onload = function () {
 function main() {
   // select all elements
   const lightSwitch = document.getElementById("lightSwitch");
+  const my_resume = document.querySelector("#my_resume");
+  const my_resume_btn = my_resume.querySelectorAll(".resume_btn");
+  const my_resume_show = my_resume.querySelectorAll("#resume_show .row");
 
+  // *****************======####======*****************
   // handle and connect all elements
   lightSwitch.addEventListener("click", handleLightSwitch(logoForDark));
   if (localStorage.getItem("lightSwitch") === "dark") {
@@ -14,6 +18,11 @@ function main() {
   } else {
     logoForDark(false);
   }
+
+  // handle my_resume
+  my_resume_btn[0].addEventListener("click", handleSkill(my_resume_show));
+  my_resume_btn[1].addEventListener("click", handleExperiences(my_resume_show));
+  my_resume_btn[2].addEventListener("click", handleEducation(my_resume_show));
 }
 
 /**
@@ -43,4 +52,31 @@ function logoForDark(isDark) {
       item.src = "./assets/images/logo-for-white-mode.svg";
     });
   }
+}
+
+/**
+ *
+ * my_resume evenHandler
+ *
+ */
+function handleSkill(my_resume_show) {
+  return function () {
+    my_resume_show[0].classList.remove("d-none");
+    my_resume_show[1].classList.add("d-none");
+    my_resume_show[2].classList.add("d-none");
+  };
+}
+function handleExperiences(my_resume_show) {
+  return function () {
+    my_resume_show[0].classList.add("d-none");
+    my_resume_show[1].classList.remove("d-none");
+    my_resume_show[2].classList.add("d-none");
+  };
+}
+function handleEducation(my_resume_show) {
+  return function () {
+    my_resume_show[0].classList.add("d-none");
+    my_resume_show[1].classList.add("d-none");
+    my_resume_show[2].classList.remove("d-none");
+  };
 }
